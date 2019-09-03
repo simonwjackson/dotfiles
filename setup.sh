5a3769dfba8b05d72f66fb711c54bdf298224ead
Simon W. Jackson <hello@simonwjackson.io>
2019-09-03 15:24:57 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+cd ~ 
+
+# Start a new git repository
+git init
+
+# Add the remote dotfiles repo
+git remote add origin https://github.com/simonwjackson/dotfiles.git
+
+# Pull and merge with local git
+git pull origin master
+
+# Install Apps
+pacman -S - < ~/PACKAGES
