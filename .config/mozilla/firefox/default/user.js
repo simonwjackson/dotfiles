90c76bb34ffe65e1f8d28649768feb578aa2a455
Simon W. Jackson <hello@simonwjackson.io>
2019-09-06 15:26:18 -0500

    add FF user preference file

---
+++ .config/mozilla/firefox/default/user.js
@@ -0,0 +1,4 @@
+user_pref("layout.css.devPixelsPerPx", "2.25");
+user_pref("full-screen-api.transition-duration.enter", "0 0");
+user_pref("full-screen-api.transition-duration.leave", "0 0");
+user_pref("browser.startup.homepage", "https://blank.org");
