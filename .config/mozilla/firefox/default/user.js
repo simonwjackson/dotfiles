5343274970db442d38a7223cde42a244de53e7d2
Simon W. Jackson <hello@simonwjackson.io>
2019-09-28 09:34:37 -0500

    some sane firefox defaults

--- .config/mozilla/firefox/default/user.js
+++ .config/mozilla/firefox/default/user.js
@@ -1,4 +1,6 @@
-// user_pref("layout.css.devPixelsPerPx", "2.25");
 user_pref("full-screen-api.transition-duration.enter", "0 0");
 user_pref("full-screen-api.transition-duration.leave", "0 0");
 user_pref("browser.startup.homepage", "https://blank.org");
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", false);
+user_pref("browser.tabs.warnOnClose", false);
+user_pref("browser.tabs.warnOnCloseOtherTabs", false);
