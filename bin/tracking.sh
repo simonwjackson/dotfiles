12bbda205d8b95fa520c19d0640b6211e9b4adec
Simon W. Jackson <hello@simonwjackson.io>
2019-10-08 13:56:04 -0500

    use i3blocks

---
+++ bin/tracking.sh
@@ -0,0 +1,1 @@
+if [[ $(timew | head -n 1 | sed -r 's/.+?"(.+?)".*/\1/') == "There is no active time tracking." ]]; then echo "░▒▓ ╳ NOT TRACKING TIME ╳ ▓▒░"; else echo "Task: $(timew | head -n 1 | sed -r 's/.+?"(.+?)".*/\1/')"; fi
