3729ccf6699f74a35b6607738e3b6c4adb02dab9
Simon W. Jackson <hello@simonwjackson.io>
2019-10-08 14:14:44 -0500

    enable bluetooth after system install

--- bin/setup/services.sh
+++ bin/setup/services.sh
@@ -4,6 +4,10 @@
 sudo systemctl enable sshd
 sudo systemctl start sshd
 
+# Enable bluetooth
+systemctl enable bluetooth.service
+systemctl start bluetooth.service
+
 # Bugwarrior
 systemctl --user enable bugwarrior-pull.timer
 systemctl --user start bugwarrior-pull.timer
