b8470c62ab59d6551fbbef11656a25224a1141aa
Simon W. Jackson <hello@simonwjackson.io>
2020-06-07 20:59:45 -0500

    Update setup.sh

--- bin/setup/setup.sh
+++ bin/setup/setup.sh
@@ -63,8 +63,7 @@ pacman \
   --noconfirm \
   pacman-contrib
 inform "Finding the fastest Arch mirrors.. This may take a few moments.."
-curl -s "https://www.archlinux.org/mirrorlist/?country=FR&country=GB&protocol=https&use_mirror_status=on" \
-| sed -e 's/^#Server/Server/' -e '/^#/d' \
+cat etc/pacman.d/mirrorlist \
 | rankmirrors -n 5 - \
 | tee /etc/pacman.d/mirrorlist
 
