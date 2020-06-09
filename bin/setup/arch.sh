a1850787dd6517d5dcf7f55bfa3321e30162ddf2
Simon W. Jackson <hello@simonwjackson.io>
2020-06-09 15:36:57 -0500

    Update arch.sh

--- bin/setup/arch.sh
+++ bin/setup/arch.sh
@@ -1,26 +1,5 @@
 #!/bin/sh
 
-# YAY is a dependency
-if ! [ -x "$(command -v yay)" ]; then
-  pacman \
-    --sync \
-    --needed \
-    --noconfirm \
-      libffi base-devel procps-ng go
-      
-  inform "Installing yay"
-  sudo -u "${USER}" bash -c '\
-    TMP_YAY=$(mktemp --directory); \
-    git clone https://aur.archlinux.org/yay.git "${TMP_YAY}"; \
-    cd "${TMP_YAY}"; \
-    makepkg \
-      --syncdeps \
-      --install \
-      --noconfirm; \
-    rm -rdf "${TMP_YAY}"; \
-  '
-fi
-
 # Install all packaged located in
 # ${HOME}/.config/packages/arch
 sudo -u "${USER}" yay \
