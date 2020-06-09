f145f7439e578564bbec41bcf4af94f0744c95ea
Simon W. Jackson <hello@simonwjackson.io>
2020-06-09 15:47:48 -0500

    Update init.sh

--- bin/setup/user/init.sh
+++ bin/setup/user/init.sh
@@ -1,26 +1,3 @@
-GIT_HOME="${HOME}/.git"
-
-if git -C "${HOME}" rev-parse --git-dir > /dev/null 2>&1; then
-  if ! [ -z "$(git --git-dir "${GIT_HOME}" status --untracked-files=no --porcelain)"]; then
-    echo "${HOME} is not clean! exiting.."
-    exit 1
-  fi
-fi
-
-git config --global user.name "${GIT_NAME}"
-git config --global user.email "${GIT_EMAIL}"
-git config --global github.user "${GITHUB_USERNAME}"
-
-TMP_DOTFILES=$(mktemp --directory)
-git clone https://github.com/simonwjackson/dotfiles.git "${TMP_DOTFILES}"
-
-rm -rdf "${GIT_HOME}"
-rsync --verbose --archive --recursive "${TMP_DOTFILES}/" "${HOME}"
-
-rm -rdf ${TMP_DOTFILES}
-
-success "Dotfiles have been cloned!"
-
 source "${HOME}/bin/setup/arch.sh"
 source "${HOME}/bin/setup/neovim.sh"
 
