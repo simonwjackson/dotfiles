1aecbfd72b43e691d0f6d08aba7dfd046e8aad67
Simon W. Jackson <simon@simonwjackson.com>
2021-09-09 23:11:31 -0500

    xhore: big update

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -29,6 +29,8 @@ config.set('auto_save.session', True)
 config.unbind('H')
 config.unbind('L')
 
+config.bind(';v', 'hint links spawn --detach mpv-video "{hint-url}"')
+
 config.bind('h', 'back')
 config.bind('l', 'forward')
 config.bind(' bu', 'spawn --userscript qutewarden user')
@@ -36,13 +38,19 @@ config.bind(' bp', 'spawn --userscript qutewarden pass')
 config.bind(' bb', 'spawn --userscript qutewarden both')
 
 config.set('url.searchengines', {
-    "DEFAULT": "https://www.google.com.ar/search?q={}"
+    "DEFAULT": "https://www.google.com.ar/search?q={}",
+    "fake": 'https://fakespot.com/analyze?url={}',
+    "r": 'https://www.reddit.com/r/{}/',
 })
 
 # More sites are responsive with this user agent
 config.set('content.headers.user_agent', "Mozilla/5.0 (Android 10; Mobile; rv:86.0) Gecko/86.0 Firefox/86.0")
 
+# Fix reddit
+config.set('content.headers.user_agent', "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36", "https://*.reddit.com/*")
+
 # Fix google account login
 config.set('content.headers.user_agent', "Mozilla/5.0 ({os_info}; rv:90.0) Gecko/20100101 Firefox/90.0", "https://accounts.google.com/*")
 
+# Desktop google drive
 config.set('content.headers.user_agent', "Mozilla/5.0 ({os_info}; rv:90.0) Gecko/20100101 Firefox/90.0", "https://drive.google.com/*")
--- .profile
+++ .profile
@@ -1,16 +1,4 @@
-export PATH=$( find "${HOME}/.local/scripts" -type d -printf ":%p" ):$PATH
-export BW_SESSION="$(cat "${HOME}/.secrets/bitwarden/session")"
-export BSPWM_SOCKET="/tmp/bspwm-socket"
-export BROWSER="$(which qutebrowser)"
-export EDITOR="$(which nvim)"
-
-export GDK_SCALE=2
-export GDK_DPI_SCALE=0.5
-export QT_AUTO_SCREEN_SET_FACTOR=0
-export QT_SCALE_FACTOR=2
-export QT_FONT_DPI=96
-
-export POLYBAR_HEIGHT=60
+[[ -e "${HOME}/.zshenv" ]] && source "${HOME}/.zshenv"
 
 if [ -z "${DISPLAY}" ] \
     && [ "${XDG_VTNR}" -eq 1 ]; then
